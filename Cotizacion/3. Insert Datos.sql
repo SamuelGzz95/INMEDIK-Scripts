7544a00a819099a37b371a7fd02b436ccac5d4df
Samuel Gzz <samuel_gzz_95@hotmail.com>
2023-12-11 22:37:40 -0600

    chore: se agrego datos iniciales de cotizacion

---
+++ Cotizacion/3. Insert Datos.sql
@@ -0,0 +1,11 @@
+INSERT INTO EstatusCotizacion(Id,Nombre,Prioridad)
+VALUES(1,'Pendiente',1);
+
+INSERT INTO EstatusCotizacion(Id,Nombre,Prioridad)
+VALUES(2,'Cotizada',2);
+
+INSERT INTO EstatusCotizacion(Id,Nombre,Prioridad)
+VALUES(3,'Cancelada',3);
+
+INSERT INTO EstatusCotizacion(Id,Nombre,Prioridad)
+VALUES(4,'Facturada',4);
